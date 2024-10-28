4e4c8104a7a5e41ea0b6cb63dba886bd8af4f939
zhao <2021454849@qq.com>
2024-10-28 19:36:06 +0800

    修改名字

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,9 +1,37 @@
 export default defineAppConfig({
-  pages: ["pages/index/index"],
+  pages: ["pages/index/index", "pages/my/my", "pages/contribute/contribute", "pages/iogin/login"],
   window: {
     backgroundTextStyle: "light",
     navigationBarBackgroundColor: "#fff",
     navigationBarTitleText: "WeChat",
     navigationBarTextStyle: "black",
   },
+  tabBar: {
+    color: '#333',
+    selectedColor: '#1296db',
+    backgroundColor: '#fff',
+    borderStyle: 'white',
+    list: [
+      {
+        text: '首页',
+        pagePath: 'pages/index/index',
+        iconPath: 'static/tabs/home_default.png',
+        selectedIconPath: 'static/tabs/home_selected.png'
+
+      },
+      {
+        text: '投稿',
+        pagePath: 'pages/contribute/contribute',
+        iconPath: 'static/tabs/contribute_default.png',
+        selectedIconPath: 'static/tabs/contuibute_selected.png'
+      }
+      {
+        text: '我的',
+        pagePath: 'pages/my/my',
+        iconPath: 'static/tabs/my_default.png',
+        selectedIconPath: 'static/tabs/my_selected.png'
+      }
+    ]
+
+  }
 });
---
+++ src/pages/my/my.tsx
@@ -0,0 +1,14 @@
+import { Component, PropsWithChildren } from 'react'
+import { View, Text } from '@tarojs/components'
+import { AtButton } from 'taro-ui'
+
+import "taro-ui/dist/style/components/button.scss" // 按需引入
+import './index.scss'
+
+export default function Contribute() {
+  return (
+    <View>
+      <Text>Hello world!</Text>
+    </View>
+  );
+}
